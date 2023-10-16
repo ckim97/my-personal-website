c873adc5aa2cab6b86625f6829aaf348f7aebec7
Christopher <chrisjwkim97@gmail.com>
2023-10-16 16:13:31 -0400

    progress

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function addingEventListener() {
+    const input = document.getElementById('nba');
+    function clickAlert() {
+    alert('NY Knicks!');
+}
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener();
+
+function addingEventListener1() {
+    const input = document.getElementById('player');
+    function clickAlert() {
+    alert('Rajon Rando');
+}
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener1();
+
+function addingEventListener2() {
+    const input = document.getElementById('series');
+    function clickAlert() {
+    alert('Eragon by Christopher Paolini');
+}
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener2();
+
+function addingEventListener3() {
+    const input = document.getElementById('nfl');
+    function clickAlert() {
+    alert('NY Jets!');
+}
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener3();
