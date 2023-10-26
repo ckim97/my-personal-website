51a4cbf023e5166e6da928d30d10e08f0bce18c5
Christopher <chrisjwkim97@gmail.com>
2023-10-26 19:23:21 -0400

    progress

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    addingMouseOverandOut();
+    addingEventListener("nba", "NY Knicks");
+    addingEventListener("player", "Rajon Rondo");
+    addingEventListener("series", "Eragon by Christopher Paolini");
+    addingEventListener("nfl", "NY Jets");
+});
+
+function addingMouseOverandOut() {
+    const words = document.getElementsByClassName("text");
+    function changeColor() {
+        this.style.color = "yellow"
+    }
+    function changeColorBack() {
+        this.style.color = ""
+    }
+    for (let i = 0; i < words.length; i++) {
+        words[i].addEventListener("mouseover", changeColor);
+        words[i].addEventListener("mouseout", changeColorBack);
+}
+}
+
 function addingEventListener(identification, message) {
     const input = document.getElementById(identification);
     function clickAlert() {
     alert(message);
 }
-    input.addEventListener('click', clickAlert);
+    input.addEventListener("click", clickAlert);
 }
 
-addingEventListener("nba", "NY Knicks");
-addingEventListener("player", "Rajon Rondo");
-addingEventListener("series", "Eragon by Christopher Paolini");
-addingEventListener("nfl", "NY Jets");
+
